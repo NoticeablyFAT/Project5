d58942073acce18037b8e19009e183482fad6c05
Sam Billerbeck <samuel.b.billerbeck-1@ou.edu>
2019-12-01 20:52:39 -0600

    Created MesoFileReader class, which will read the mesonet.txt file and return an arraylist containing the file's information

---
+++ MesoFileReader.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MesoFileReader {
+
+	private ArrayList<String> stations;
+	
+	
+}
