31ce6d0436ac76aac52a195d3a38da04ba0e5163
Sam Billerbeck <samuel.b.billerbeck-1@ou.edu>
2019-12-02 13:17:42 -0600

    Added slider to allow hamming distance to be chosen

--- MesoGUI.java
+++ MesoGUI.java
@@ -1,6 +1,8 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -10,15 +12,32 @@ public class MesoGUI extends Application {
 	public void start(Stage mesoStage) throws Exception {
 		Scene scene = null;
 		Pane pane = null;
+		GridPane gridPane = null;
 		
 		pane = new Pane();
-		scene = new Scene(pane);
+		
+		gridPane = new GridPane();
+		gridPane.setHgap(20);
+		gridPane.setVgap(20);
+		
+		scene = new Scene(gridPane);
 		TextField enterHamm = new TextField();
+		TextField enterHamm2 = new TextField();
+		Slider hammSlider = new Slider(1,4,1);
+		hammSlider.setMinorTickCount(0);
+		hammSlider.setMajorTickUnit(1);
+		hammSlider.setShowTickMarks(true);
+		hammSlider.setShowTickLabels(true);
+		hammSlider.setSnapToTicks(true);
 		
 		enterHamm.setText("Enter Hamming Dist:");
+		enterHamm2.setText("Enter Hamming Dist:");
+
 		enterHamm.setEditable(false);
 		//enterHamm.setPrefColumnCount(2);
-		pane.getChildren().add(enterHamm);
+		gridPane.add(enterHamm, 0, 0);
+		gridPane.add(enterHamm2, 20, 20);
+		gridPane.add(hammSlider, 0, 1);
 		
 		
 		
