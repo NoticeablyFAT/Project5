e4eb0548c4ba20abf0cd6072683307c5a72c6cef
Sam Billerbeck <samuel.b.billerbeck-1@ou.edu>
2019-12-02 12:24:12 -0600

    Added main and start methods, and created simple test scene with text box.

--- MesoGUI.java
+++ MesoGUI.java
@@ -1,9 +1,51 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
-public class MesoGUI {
+public class MesoGUI extends Application {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	@Override
+	public void start(Stage mesoStage) throws Exception {
+		Scene scene = null;
+		Pane pane = null;
+		
+		pane = new Pane();
+		scene = new Scene(pane);
+		TextField enterHamm = new TextField();
+		
+		enterHamm.setText("Enter Hamming Dist:");
+		enterHamm.setEditable(false);
+		//enterHamm.setPrefColumnCount(2);
+		pane.getChildren().add(enterHamm);
+		
+		
+		
+		mesoStage.setScene(scene);
+		mesoStage.setTitle("Test");
+		mesoStage.show();
+		
+		
+		
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	
 
 }
