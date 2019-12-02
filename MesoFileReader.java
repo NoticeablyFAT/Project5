37525b7c1719a5170bdb1e4d38b7dca0726c0434
Sam Billerbeck <samuel.b.billerbeck-1@ou.edu>
2019-12-01 21:11:03 -0600

    Added default constructor, which reads file and creates an arraylist.

--- MesoFileReader.java
+++ MesoFileReader.java
@@ -6,6 +6,31 @@ import java.io.IOException;
 public class MesoFileReader {
 
 	private ArrayList<String> stations;
+	private String fileName = "Mesonet.txt";
+
+	public MesoFileReader() {
+		stations = new ArrayList<String>();
+		String line = "";
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(fileName)); // Initializes buffered reader to read file
+																				
+			line = br.readLine();
+			while (line != null) { // Reads file and adds all station ID's to strings arraylist
+
+				stations.add(line.substring(0, 4));
+				line = br.readLine();
+
+			}
+			br.close();
+		} catch (IOException e) { // Catches exception if file is not found
+			System.out.println("File not found.");
+		}
+
+	}
 	
 	
+	
+	
+
 }
