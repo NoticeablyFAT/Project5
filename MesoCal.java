3de2324b01dea58a65096d6eb1e27e1cea68a765
Sam Billerbeck <samuel.b.billerbeck-1@ou.edu>
2019-12-03 13:33:48 -0600

    Created MesoCal constructor, which will perform calculations and return an arraylist of stations that match the desired hamming distance

--- MesoCal.java
+++ MesoCal.java
@@ -1,8 +1,36 @@
+import java.util.ArrayList;
 
 public class MesoCal {
 
-	
-	
-	
-	
+	public ArrayList<String> findHammStations(int hammDistance, String station) {
+		MesoFileReader fullList = new MesoFileReader();
+		ArrayList<String> allStations = fullList.getStations();
+		ArrayList<String> hammStations = new ArrayList<String>();
+
+		char[] charStation = station.toCharArray(); // Creates char arrays to compare strings
+
+		for (int i = 0; i < allStations.size(); ++i) {
+
+			int hammRelative = 0;
+
+			char[] charInput1Init = new char[4];
+
+			charInput1Init = allStations.get(i).toCharArray();
+
+			for (int j = 0; j < charStation.length; ++j) { // Finds hamming distance for station relative to input
+				if (charInput1Init[j] != charStation[j]) {
+
+					++hammRelative;
+				}
+
+			}
+			if (hammRelative == hammDistance) {
+				hammStations.add(allStations.get(i));
+			}
+
+		}
+
+		return hammStations;
+	}
+
 }
